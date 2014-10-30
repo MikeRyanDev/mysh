6d5ceb421e8eb482181a366487389f4b23198e7e
lmj0011 <lmj0011@users.noreply.github.com>
2014-10-30 16:31:12 -0500

    Create qkcompile.h
    moved to classes folder from commands

---
+++ classes/qkcompile.h
@@ -0,0 +1,47 @@
+//***********************************************************//
+//	File: qkcompile.h
+//	Author: Landan M Young Jackson
+//
+// Just my outline for now. Will most likely change
+//***********************************************************//
+#pragma once 
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <unistd.h> /*for fork*/
+//#include <sys/types.h> /*for pid_t*/
+//#include <sys/wait.h> /*for wait*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class qkcompile
+{
+private:
+
+	string _cmd; //command name; in this case the c/c++ file
+	string base_name;	// file name without the extension
+	string all_args;	//string containing all arguments seperated by whitespace
+	string all_flags;	//string containing all arguments seperated by whitespace
+	vector<string> vec_args; //vector containing the passed arguments 
+	vector<char> vec_flgs;	//vector containing the passed flags
+
+	string get_args(vector<string> arguments); //gathers all arguments into one string
+	string get_flgs(vector<char> flags); //gathers all arguments into one string
+	string get_base_name(string command); //strips the extension (.c or .cpp) from the file name
+
+
+
+public:
+
+	qkcompile(); //default contrutor
+	qkcompile( string command, vector<string> arguments, vector<char> flags );//overloaded contructor
+	~qkcompile(); //decontructor
+
+	void script_call(); //fucntion that will call on a script to perform actual compiling
+
+	
+
+
+}
