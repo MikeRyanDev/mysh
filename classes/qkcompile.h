83abaaf25120c82f34f6070acaed9f73191d8cf4
lmj0011 <lmj0011@users.noreply.github.com>
2014-11-01 00:53:31 -0500

    Update qkcompile.h
    just mirroring changes

--- classes/qkcompile.h
+++ classes/qkcompile.h
@@ -6,13 +6,17 @@
 //***********************************************************//
 #pragma once 
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-//#include <unistd.h> /*for fork*/
-//#include <sys/types.h> /*for pid_t*/
-//#include <sys/wait.h> /*for wait*/
+#inlcude <cstring>
+#include <unistd.h> /*for fork*/
+#include <sys/types.h> /*for pid_t*/
+#include <sys/wait.h> /*for wait*/
 #include <string>
 #include <vector>
+
+#define MAX_CHARS 257
 
 using namespace std;
 
@@ -22,12 +26,14 @@ private:
 
 	string _cmd; //command name; in this case the c/c++ file
 	string base_name;	// file name without the extension
+	string c_type;		//file type (c or cpp)
 	string all_args;	//string containing all arguments seperated by whitespace
 	string all_flags;	//string containing all arguments seperated by whitespace
 
-	string get_args(vector<string> arguments); //gathers all arguments into one string
-	string get_flgs(vector<char> flags); //gathers all arguments into one string
-	string get_base_name(string command); //strips the extension (.c or .cpp) from the file name
+	void set_base_name(string command); //strips the extension (.c or .cpp) from the file name
+	void get_args(vector<string> arguments); //gathers all arguments into one string
+	void get_flgs(vector<char> flags); //gathers all arguments into one string
+	void script_call();  //fucntion that will call on a script to perform actual compiling
 
 
 
@@ -36,8 +42,6 @@ public:
 	qkcompile(); //default contrutor
 	qkcompile( string command, vector<string> arguments, vector<char> flags );//overloaded contructor
 	~qkcompile(); //decontructor
-
-	void script_call(); //fucntion that will call on a script to perform actual compiling
 
 	
 
